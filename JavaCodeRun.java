17b27b8bfb03b3e5afdafa8bf1b2995b2bbddaf8
RiteshGitAccount <85227773+RiteshGitAccount@users.noreply.github.com>
2021-07-06 15:40:56 +0530

    Update JavaCodeRun.java

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,6 +2,6 @@ public class JavaCodeRun{
 
      public static void main(String []args){
         System.out.println("Hello Jenkins Test  World");
-          System.out.println("Hello    World 100");
+          System.out.println("Hello  JenkinsJenkinsJenkins  World 100");
      }
 }
