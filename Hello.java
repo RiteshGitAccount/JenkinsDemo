319e768965c713306d4b90c7fc32dc94b0e1164a
RiteshGitAccount <85227773+RiteshGitAccount@users.noreply.github.com>
2021-07-06 13:44:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 
      public static void main(String []args){
-        System.out.println("Hellotht   tuWorld");
+        System.out.println("Hellotht   World");
            System.out.println("Hello Jenkins Automatic Trigger World 700");
      }
 }
