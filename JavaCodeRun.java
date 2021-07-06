af08adc4d79da57c424e26d42a2914252811cf83
RiteshGitAccount <85227773+RiteshGitAccount@users.noreply.github.com>
2021-07-06 13:00:37 +0530

    Update JavaCodeRun.java

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,5 +2,6 @@ public class JavaCodeRun{
 
      public static void main(String []args){
         System.out.println("Hello Jenkins Automatic Trigger World");
+          System.out.println("Hello Jenkins Automatic Trigger World 2");
      }
 }
