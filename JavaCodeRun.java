5a48804be2df31a34e1f7b50ad17413d5dc2ef8a
Ritesh <riteshkaushal.jobs@gmail.com>
2021-07-08 16:50:11 +0530

    ReadMe File Updated7

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,6 +2,6 @@ public class JavaCodeRun{
 
      public static void main(String []args){
        
-           System.out.println("Hello 11th July Third  World 100");
+           System.out.println("Hello 12th July Third  World 100");
      }
 }
