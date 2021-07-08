864eb7607258736ab899e52fe7671418e623a984
Ritesh <riteshkaushal.jobs@gmail.com>
2021-07-08 16:02:21 +0530

    ReadMe File Updated4

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -3,6 +3,6 @@ public class JavaCodeRun{
      public static void main(String []args){
         System.out.println("Hello Again HELLO World");
           System.out.println("Hello   New World 100");
-           System.out.println("Hello   Third  World 100");
+           System.out.println("Hello  8th July Third  World 100");
      }
 }
--- RiteshReadMe.txt
+++ RiteshReadMe.txt
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                echo 'Building.. 8th July Third time'
+                echo 'Building.. 8th July fourth time'
             }
             
         }
