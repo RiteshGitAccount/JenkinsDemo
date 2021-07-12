793e76796d13041e31acf7201d783bb69ed1661c
RiteshGitAccount <85227773+RiteshGitAccount@users.noreply.github.com>
2021-07-12 14:26:33 +0530

    Update JavaCodeRun.java

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,6 +2,6 @@ public class JavaCodeRun{
 
      public static void main(String []args){
        
-           System.out.println("Hello 9th July Check Test 282tgtg25");
+           System.out.println("Hello 12th July Check Test ");
      }
 }
