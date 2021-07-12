9929b8dedc79ddc90d8f42e158c9748cd0bd42e1
Ritesh <riteshkaushal.jobs@gmail.com>
2021-07-12 16:26:27 +0530

    123

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,6 +2,6 @@ public class JavaCodeRun{
 
      public static void main(String []args){
        
-           System.out.println("Hello 12th July Check Test 12th 2021");
+           System.out.println("Hello 12th July Check Test 12th 2021 1st");
      }
 }
