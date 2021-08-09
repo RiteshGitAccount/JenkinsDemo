1ae9e50ecae5793e44591c0cfe5eec6d783ba5f1
Ritesh <riteshkaushal.jobs@gmail.com>
2021-08-09 10:32:15 +0530

    Update

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,9 +2,9 @@ public class JavaCodeRun{
 
      public static void main(String []args){
 
-        System.out.println("Hello test World Test This 12 July 2021");
+        System.out.println("Hello test World Test This 9th Aug 2021");
        
-           System.out.println("Hello 4th Aug 2021 1st Its Working Hurry I am Happy Shukriya. Thank you !!. Remove the unwanted code");
+           System.out.println("Hello 9th Aug 2021. Hello Vineet. ");
 
      }
 }
