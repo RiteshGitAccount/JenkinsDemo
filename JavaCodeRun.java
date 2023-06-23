2063cda47b37ad60534bb3e9d4d47bc26a606f5c
RiteshGitAccount <85227773+RiteshGitAccount@users.noreply.github.com>
2023-06-23 16:43:36 +0530

    Update JavaCodeRun.java

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,7 +2,7 @@ public class JavaCodeRun{
 
      public static void main(String []args){
 
-        System.out.println("Hello 23rd June 2023 Testing  it configured ");
+        System.out.println("Hello Webhook Configued 23rd June 2023 Testing  it configured ");
        
            System.out.println("Hello 27 Oct 2021 Bye");
 
