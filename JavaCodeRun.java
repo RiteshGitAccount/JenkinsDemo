5c9515a4321a6090727a47d4eefe5a79f48db9db
RiteshGitAccount <85227773+RiteshGitAccount@users.noreply.github.com>
2023-06-23 16:40:29 +0530

    Update JavaCodeRun.java

--- JavaCodeRun.java
+++ JavaCodeRun.java
@@ -2,7 +2,7 @@ public class JavaCodeRun{
 
      public static void main(String []args){
 
-        System.out.println("Hello 23rd June 2023 Testing Finally it got configured ");
+        System.out.println("Hello 23rd June 2023 Testing Finally it configured ");
        
            System.out.println("Hello 27 Oct 2021 Bye");
 
